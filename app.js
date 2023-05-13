fca094fed3b45e7c97c3fce8873424e119f4260a
Guillermo Santolaya <guillermosantolaya.93@gmail.com>
2023-05-13 16:56:47 -0300

    modificamos app.js -configuramos puerto - creamos variables de entorno - conectamos con la db

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 8080
+
+const cors = require('cors');
+const { initDBConnection } = require('./database/dbConnection');
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res)=>{
+    res.send({ mensaje: "Hola humano" })
+})
+
+app.listen(PORT, ()=>{
+    initDBConnection();
+    console.log(`Escuchando en puerto ${PORT}`)
+})
